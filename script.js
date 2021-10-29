75feebc301ebd4d62abc067c395e1100d6fad49a
Cristina <cristoshiwassaki@gmail.com>
2021-10-29 20:35:06 -0300

    js para mudança do texto

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const bgColor = document.getElementsByClassName('body')[0];
 const img = document.getElementById('figure');
-const text = document.querySelector('.status').value;
+const text = document.querySelector('.status');
+const text2 = document.querySelector('.age');
 
 
 function changeAvatar(){
-  bgColor.style.backgroundColor = "#e40043";
+
+  if (img.src.match("./images/monica_baby.png")) {
+    bgColor.style.backgroundColor = '#ff5a5f';
+    img.setAttribute('src', './images/monica.png');
+    text.innerHTML = '<strong>Estágio</strong>: criança';
+    text2.innerHTML = '<strong>Idade</strong>: 7 anos'
+  } 
+
+  else if (img.src.match('./images/monica.png')){
+    bgColor.style.backgroundColor = "#e40043";  
+    img.setAttribute('src', './images/monica_jovem.png');
+    text.innerHTML = '<strong>Estágio</strong>: jovem';
+    text2.innerHTML = '<strong>Idade</strong>: 16 anos';
+  } 
   
-  img.setAttribute('src', './images/monica_jovem.png');
-  
-  text.innerHTML = "Estágio: Baby";
+  else if (img.src.match('./images/monica_jovem.png')){
+    bgColor.style.backgroundColor = '#fe9ac7';
+    img.setAttribute('src', './images/monica_baby.png');
+    text.innerHTML = '<strong>Estágio</strong>: bebê';
+    text2.innerHTML = '<strong>Idade</strong>: 1 ano';
+  } 
+
 }
